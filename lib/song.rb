555a91409fa2ecd80457210be1192ac86fc8a6ab
Shoptorshi Jamil <sjami002@gmail.com>
2019-04-07 00:26:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,8 @@
 class Song
   attr_accessor :title, :artist
   
-  def initialize(title, artist)
-    @title = title
-    @artist = artist
-  end
+  # def initialize(title, artist)
+  #   @title = title
+  #   @artist = artist
+  # end
 end
